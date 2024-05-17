2d3c299ca60b593e6cb3fe6860a459476605d947
Herman <hermanvasilkouskiy@gmail.com>
2024-05-18 02:14:18 +0300

    feat: implement not-found page

--- src/app/pages/page-not-found/page-not-found.tsx
+++ src/app/pages/page-not-found/page-not-found.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import classes from './page-not-found.module.scss';
+import notFound from '../../../assets/not-found.svg';
+import logo from '../../../assets/arrowFlicks-logo.svg';
+import PrimaryButton from '../../components/button/primary-button';
 
 function PageNotFound() {
-  return <div className={classes['page-not-found']}>Page not found</div>;
+  return (
+    <main className={classes['page-not-found']}>
+      <Link to="/">
+        <img src={logo} className={classes.logo} alt="arrowFlicks" />
+      </Link>
+      <div className={classes.wrapper}>
+        <img src={notFound} alt="Page not found" />
+        <div className={classes.info}>
+          <span className={classes.text}>We can’t find the page you are looking for</span>
+          <PrimaryButton to="/">go home</PrimaryButton>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default PageNotFound;
--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mantine/core';
 const theme = createTheme({
   colors: {
     purple: ['', '#F2ECFA', '#E5D5FA', '#D1B4F8', '#BD93F7', '#9854F6', '#541F9D', '', '', ''],
-    grey: ['', '#FFFFFF', '#F5F5F6', '#EAEBED', '#D5D6DC', '#ACADB9', '#7B7C88', '#232134', '', ''],
+    grey: ['#FFFFFF', '#F5F5F6', '#EAEBED', '#D5D6DC', '#ACADB9', '#7B7C88', '#232134', '', '', ''],
   },
   primaryColor: 'purple',
   primaryShade: 5,
